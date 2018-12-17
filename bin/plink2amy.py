12973383284a49be84df1bc811d94d76de35a6e5
shaze <scott.hazelhurst@wits.ac.za>
2018-12-17 22:27:20 +0200

    added own conversion

---
+++ bin/plink2amy.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from pandas_plink import read_plink
+import sys
+
+
+(bim,fam,bed) =read_plink(sys.argv[1],verbose=False)
+
+bed = bed.compute()
+
+
+
+def getInd(p):
+    gt = bed[:,p]
+    def cvt(s):
+        pref = "chrY\t%d\t%s\t%s\t%s\n"
+        name = "x" if len(bim.snp[s])==0 else bim.snp[s]
+        if gt[s]==0:
+            return pref%(bim.pos[s],bim.a1[s],bim.a0[s],name)
+        elif gt[s]==1:
+            return pref%(bim.pos[s],bim.a1[s],bim.a0[s],name)
+        elif gt[s]==2:
+            return pref%(bim.pos[s],bim.a1[s],".",name)
+        else:
+            return pref%(bim.pos[s],".",".",name)            
+    res =  list(map(cvt,range(len(bim))))
+    return res
+
+for p in range(len(fam)):
+    if fam.fid[p]==fam.iid[p]:
+        the_id = fam.fid[p]
+    else:
+        the_id = fam.fid[p]+"_"+fam.iid[p]
+    g=open("%s.amy"%the_id,"w")
+    g.writelines(getInd(p))
+    g.close()
